fd8c5dffbdea092b0c221f29054239736fc44443
Blade21Shade <blade21shade@gmail.com>
2025-07-17 21:26:30 -0500

    Make basic logic for setting the operator and operands and the basic functions for evaluating operations
    I haven't tested anything yet, so I'm unsure if the switch statement on currentOperation will work as I am expecting or if I will need to do some different object based stuff.
    
    There were also some changes to capitalization to make everything in Operations all caps instead of all lowercased.
    
    index.js
    - Create the operate() function which  calls the various evaluation functions based on currentOperation's value
     - add(), sub(), mul(), and div()
      - div() checks for dividing by 0
    - There is a function for setting an operand and one for setting the operator
     - setOperand() just takes the value and a boolean forFirstOperand
     - setOperator uses a switch statement to set currentOperation to one of the values of Operations based on the character
     - +, -, *, and /

--- index.js
+++ index.js
@@ -1,13 +1,88 @@
-// Data the calculator will use to conduct operations
-let firstVal = 0;
-let secondVal = 0;
+// Data
+let firstOperand = 0;
+let secondOperand = 0;
 let computedVal = 0;
 
 const Operations = Object.freeze({
-    ADD: "add",
-    SUB: "sub",
-    MUL: "mul",
-    DIV: "div",
+    NOTSET: "NOTSET", // This is set when an operation finishes and used before the first run; it prevents chained operations from occurring
+    ADD: "ADD",
+    SUB: "SUB",
+    MUL: "MUL",
+    DIV: "DIV",
 });
 
-let currentOperation = Operations.ADD;
+const currentOperation = Operations.NOTSET;
+
+// Functions
+// Operation functions
+function operate() {
+    switch(currentOperation) {
+        case "ADD":
+            add();    
+            break;
+        case "SUB":
+            sub();
+            break;
+        case "MUL":
+            mul();
+            break;
+        case "DIV":
+            div();
+            break;
+        case "NOTSET":
+            console.log("NOTSET called in operate: This shouldn't happen")
+            break;
+    }
+}
+
+function add() {
+    computedVal = firstOperand + secondOperand;
+}
+
+function sub() {
+    computedVal = firstOperand - secondOperand;
+}
+
+function mul() {
+    computedVal = firstOperand * secondOperand;
+}
+
+function div() {
+    if (secondOperand == 0) {
+        alert("Cannot divide by 0")
+        return;
+    }
+    computedVal = firstOperand / secondOperand;
+}
+
+// Setting functions
+function setOperator(operator) {
+    if (currentOperation !== "NOTSET") { // If this is in the middle of parsing the string any operators after the first are ignored
+        return;
+    }
+    switch(operator) {
+        case "+":
+            currentOperation = Operations.ADD;
+            break;
+        case "-":
+            currentOperation = Operations.SUB;
+            break;
+        case "*":
+            currentOperation = Operations.MUL;
+            break;
+        case "/":
+            currentOperation = Operations.DIV;
+            break;
+        default: 
+            alert("Issue when setting operator")
+            break;
+    }
+}
+
+function setOperand(value, forFirstOperand) {
+    if (forFirstOperand) {
+        firstOperand = value;
+    } else {
+        secondOperand = value;
+    }
+}
